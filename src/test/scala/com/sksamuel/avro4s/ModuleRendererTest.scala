fa3045aba9b2994912464eb7f75b0c6afe0fa065
Jean Helou <jean.helou@gmail.com>
2017-09-26 13:20:00 +0200

    [Boyscout] fix failing test cases
    It seems the tests are failing because the actual code generates a
    companion object with a helpful type alias. Since this seems both useful
    and benign I adjusted the test case to match

--- src/test/scala/com/sksamuel/avro4s/ModuleRendererTest.scala
+++ src/test/scala/com/sksamuel/avro4s/ModuleRendererTest.scala
@@ -57,9 +57,10 @@ class ModuleRendererTest extends WordSpec with Matchers {
       new ModuleRenderer()(RecordType("com.sammy", "MyClass", Seq(FieldDef("name", UnionType(NullType, PrimitiveType.Int, PrimitiveType.Boolean))))) shouldBe "//auto generated code by avro4s\ncase class MyClass(\n  name: Option[Either[Int, Boolean]]\n)"
     }
     "generate coproducts for union types of 3+ non-null types" in {
-      new ModuleRenderer()(RecordType("com.sammy", "MyClass", Seq(FieldDef("name", UnionType(PrimitiveType.String, PrimitiveType.Int, PrimitiveType.Boolean))))) shouldBe "//auto generated code by avro4s\ncase class MyClass(\n  name: shapeless.:+:[String, shapeless.:+:[Int, shapeless.:+:[Boolean, shapeless.CNil]]]\n)"
+      new ModuleRenderer()(RecordType("com.sammy", "MyClass", Seq(FieldDef("name", UnionType(PrimitiveType.String, PrimitiveType.Int, PrimitiveType.Boolean))))) shouldBe "//auto generated code by avro4s\ncase class MyClass(\n  name: shapeless.:+:[String, shapeless.:+:[Int, shapeless.:+:[Boolean, shapeless.CNil]]]\n)\n\n//auto generated code by avro4s\nobject MyClass {\n  type NameType = shapeless.:+:[String, shapeless.:+:[Int, shapeless.:+:[Boolean, shapeless.CNil]]]\n}"
     }
     "generate Option[coproducts] for union types of 3+ non-null types with null" in {
-      new ModuleRenderer()(RecordType("com.sammy", "MyClass", Seq(FieldDef("name", UnionType(NullType, PrimitiveType.String, PrimitiveType.Int, PrimitiveType.Boolean))))) shouldBe "//auto generated code by avro4s\ncase class MyClass(\n  name: Option[shapeless.:+:[String, shapeless.:+:[Int, shapeless.:+:[Boolean, shapeless.CNil]]]]\n)"
+      val actual = new ModuleRenderer()(RecordType("com.sammy", "MyClass", Seq(FieldDef("name", UnionType(NullType, PrimitiveType.String, PrimitiveType.Int, PrimitiveType.Boolean)))))
+      actual shouldBe "//auto generated code by avro4s\ncase class MyClass(\n  name: Option[shapeless.:+:[String, shapeless.:+:[Int, shapeless.:+:[Boolean, shapeless.CNil]]]]\n)\n\n//auto generated code by avro4s\nobject MyClass {\n  type NameType = Option[shapeless.:+:[String, shapeless.:+:[Int, shapeless.:+:[Boolean, shapeless.CNil]]]]\n}"
     }}
 }
